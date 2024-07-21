35e5ae3b05ab05f91a0517c8fd05e65c093a7ce4
emboy <95904552+emboyip@users.noreply.github.com>
2024-07-21 13:39:58 +0330

    Update var.py

--- var/var.py
+++ var/var.py
@@ -1,4 +1,4 @@
-url = "https://1cfa-91-107-149-108.ngrok-free.app"
-api_id = 14428638
-api_hash = "cbf99b5c5298e28747eeed1516180cfa"
-bot_token = "7181042991:AAGTrjmkTCa8Mj-BNgd7_wSLrjdFY_9W8FI"
+url = ""
+api_id = 333
+api_hash = ""
+bot_token = ""
